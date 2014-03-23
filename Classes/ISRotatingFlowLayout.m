10efb3a0bea3fdcc2e8abd8aea3965ff99a3c662
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-03-23 21:35:51 +0100

    Experimental fixing of headers. Needs work.

--- Classes/ISRotatingFlowLayout.m
+++ Classes/ISRotatingFlowLayout.m
@@ -45,19 +45,20 @@
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
 {
-  if ((self.scrollDirection ==
-       UICollectionViewScrollDirectionVertical &&
-       self.currentBounds.size.width !=
-       newBounds.size.width) ||
-      (self.scrollDirection ==
-       UICollectionViewScrollDirectionHorizontal &&
-       self.currentBounds.size.height !=
-       newBounds.size.height)) {
-    self.currentBounds = newBounds;
-    [self invalidateLayout];
-    return YES;
-  }
-  return [super shouldInvalidateLayoutForBoundsChange:newBounds];
+  return YES;
+//  if ((self.scrollDirection ==
+//       UICollectionViewScrollDirectionVertical &&
+//       self.currentBounds.size.width !=
+//       newBounds.size.width) ||
+//      (self.scrollDirection ==
+//       UICollectionViewScrollDirectionHorizontal &&
+//       self.currentBounds.size.height !=
+//       newBounds.size.height)) {
+//    self.currentBounds = newBounds;
+//    [self invalidateLayout];
+//    return YES;
+//  }
+//  return [super shouldInvalidateLayoutForBoundsChange:newBounds];
 }
 
 
@@ -149,6 +150,80 @@
   }
   
 }
+
+
+#pragma mark STICKY HEADERS CODE BELOW
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+  NSMutableArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
+  
+  NSMutableIndexSet *missingSections = [NSMutableIndexSet indexSet];
+  for (NSUInteger idx=0; idx<[answer count]; idx++) {
+    UICollectionViewLayoutAttributes *layoutAttributes = answer[idx];
+    
+    if (layoutAttributes.representedElementCategory == UICollectionElementCategoryCell || layoutAttributes.representedElementCategory == UICollectionElementCategorySupplementaryView) {
+      [missingSections addIndex:(NSUInteger) layoutAttributes.indexPath.section];  // remember that we need to layout header for this section
+    }
+    if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]) {
+      [answer removeObjectAtIndex:idx];  // remove layout of header done by our super, we will do it right later
+      idx--;
+    }
+  }
+  
+  //    layout all headers needed for the rect using self code
+  [missingSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:idx];
+    UICollectionViewLayoutAttributes *layoutAttributes = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:indexPath];
+    if (layoutAttributes) {
+      [answer addObject:layoutAttributes];
+    }
+  }];
+  
+  return answer;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+  UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+  if ([kind isEqualToString:UICollectionElementKindSectionHeader]) {
+    UICollectionView * const cv = self.collectionView;
+    
+    CGFloat topOffset = 0;
+    if ([self.collectionView.dataSource isKindOfClass:[UIViewController class]]) {
+      UIViewController *collectionViewParentViewController = (UIViewController *)self.collectionView.dataSource;
+      topOffset = collectionViewParentViewController.topLayoutGuide.length;
+    }
+    
+    CGPoint const contentOffset = CGPointMake(cv.contentOffset.x, cv.contentOffset.y + topOffset);
+    CGPoint nextHeaderOrigin = CGPointMake(INFINITY, INFINITY);
+    
+    if (indexPath.section+1 < [cv numberOfSections]) {
+      UICollectionViewLayoutAttributes *nextHeaderAttributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:[NSIndexPath indexPathForItem:0 inSection:indexPath.section+1]];
+      nextHeaderOrigin = nextHeaderAttributes.frame.origin;
+    }
+    
+    CGRect frame = attributes.frame;
+    if (self.scrollDirection == UICollectionViewScrollDirectionVertical) {
+      frame.origin.y = MIN(MAX(contentOffset.y, frame.origin.y), nextHeaderOrigin.y - CGRectGetHeight(frame));
+    }
+    else {
+      frame.origin.x = MIN(MAX(contentOffset.x, frame.origin.x), nextHeaderOrigin.x - CGRectGetWidth(frame));
+    }
+    attributes.zIndex = 1024;
+    attributes.frame = frame;
+  }
+  
+  return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+  UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+  return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+  UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+  return attributes;
+}
+
 
 
 @end
